1dcab9b540f010c99682d1b714afc3d820987528
Kirsten S <kirsten@Kirstens-MacBook-Air.local>
2020-08-27 22:53:28 -0400

    made ingredients and recipes_ingredients tables in migration, ran migration

--- data/migrations/20200827222518_initial.js
+++ data/migrations/20200827222518_initial.js
@@ -19,11 +19,34 @@ exports.up = async function(knex) {
         table.integer("step_number").notNull()
     })
     // ingredients table
+    await knex.schema.createTable("ingredients", (table) => {
+        table.increments("id")
+        table.text("name").notNull().unique()
+    })
     // recipes_ingredients table
+    await knex.schema.createTable("recipes_ingredients", (table) => {
+        table
+            .integer("recipe_id")
+            .notNull()
+            .references("id")
+            .inTable("recipes")
+            .onDelete("CASCADE")
+            .onUpdate("CASCADE")
+        table
+            .integer("ingredient_id")
+            .notNull()
+            .references("id")
+            .inTable("ingredients")
+            .onDelete("CASCADE")
+            .onUpdate("CASCADE")
+        table.float("quantity").notNull()
+    })
 };
 
 exports.down = async function(knex) {
     // drop tables in reverse order of creation
+    await knex.schema.dropTableIfExists("recipes_ingredients")
+    await knex.schema.dropTableIfExists("ingredients")
     await knex.schema.dropTableIfExists("steps")
     await knex.schema.dropTableIfExists("recipes")
 };
